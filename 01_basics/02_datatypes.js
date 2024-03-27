834c100e261fabca0e67b30ee8cec40df61f73ca
Yasher Arafath <86520621+Yasher07@users.noreply.github.com>
2024-03-27 09:41:40 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3);
+console.log("Yasher")
+
+let name = "Yasher"
+let age = 24
+let isLoggedIn = false
+
+
+//PRIMITIVE DATATYPES
+//number => 2 to the power 53
+//bigint
+//string => ""
+//boolean => true/false
+// null => standalone value
+//undefined => 
+//symbol => unique
+
+
+//NON-PRIMITIVE DATATYPES
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
