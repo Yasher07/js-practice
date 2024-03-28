5ab69c0bd6ec10f9ae2794e62f52f9fbfb328dae
Yasher Arafath <86520621+Yasher07@users.noreply.github.com>
2024-03-28 08:40:55 +0000

    Datatypes Summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,40 @@
+//  Primitive  
+
+// 7 types : String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+// Javascript is dynamically typed language because the type of a variable is determined by the value that is assigned to it, and can change throughout the program as different values are assigned. There is no need to declare the type of a variable before using it.
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+
+const bigNumber = 237383837828282828828222n
+
+console.log(typeof bigNumber)
+
+
+// Reference (Non Primitive)
+
+// Array, Objects, FUnctions
+
+const heroes = ["shaktimaan", "naagraaj", "doga"]
+
+let myObj = {
+    name : "Yasher",
+    age: 22,
+}
+
+const myFunction = function (){
+    console.log("Hello World")
+}
+
+console.log(typeof heroes)
